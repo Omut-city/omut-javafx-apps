48e4ae8ce4707dcd64c96ab1194a4e4bc73df912
Alexander Pyankov <python239@mail.ru>
2025-11-28 00:06:02 +0500

    Add menu

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -2,6 +2,7 @@
 package com.example.demo.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import org.springframework.stereotype.Controller;
 
@@ -13,5 +14,29 @@ public class MainController {
     @FXML
     public void onClick() {
         label.setText("Spring + JavaFX are works!");
+    }
+
+    @FXML
+    public void onNew() {
+        label.setText("New document!");
+    }
+
+    @FXML
+    public void onOpen() {
+        label.setText("Open document!");
+    }
+
+    @FXML
+    public void onExit() {
+        System.exit(0);
+    }
+
+    @FXML
+    public void onAbout() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText(null);
+        alert.setContentText("Application JavaFX + Spring Boot");
+        alert.showAndWait();
     }
 }
